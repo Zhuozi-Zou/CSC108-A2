a021656c7d7d5d0b5ebd6a3fe8a8d6cb06cc8bd3
Fillis-Zou <34638351+Fillis-Zou@users.noreply.github.com>
2018-07-14 14:31:24 -0400

    About a system of bike stations.
    It's just some functions, not a whole program.

---
+++ bikes.py
@@ -0,0 +1,330 @@
+""" CSC108 Assignment 2 Starter code """
+
+from typing import List, TextIO
+
+# A set of constants, each representing a list index for station information.
+ID = 0
+NAME = 1
+LATITUDE = 2
+LONGITUDE = 3
+CAPACITY = 4
+BIKES_AVAILABLE = 5
+DOCKS_AVAILABLE = 6
+IS_RENTING = 7
+IS_RETURNING = 8
+
+####### BEGIN HELPER FUNCTIONS ####################
+
+def is_number(value: str) -> bool:
+    """Return True if and only if value represents a decimal number.
+
+    >>> is_number('csc108')
+    False
+    >>> is_number('  108 ')
+    True
+    >>> is_number('+3.14159')
+    True
+    """
+
+    return value.strip().lstrip('-+').replace('.', '', 1).isnumeric()
+
+
+# It isn't necessary to call this function to implement your bikes.py
+# functions, but you can use it to create larger lists for testing.
+# See the main block below for an example of how to do that.
+def csv_to_list(csv_file: TextIO) -> List[List[str]]:
+    """Read and return the contents of the open CSV file csv_file as a list of
+    lists, where each inner list contains the values from one line of csv_file.
+
+    Docstring examples not given since results depend on data to be input.
+    """
+
+    # Read and discard header.
+    csv_file.readline()
+
+    data = []
+    for line in csv_file:
+        data.append(line.strip().split(','))
+    return data
+
+
+####### END HELPER FUNCTIONS ####################
+
+### SAMPLE DATA TO USE IN DOCSTRING EXAMPLES ####
+
+SAMPLE_STATIONS = [
+    [7087, 'Danforth/Aldridge', 43.684371, -79.316756, 23, 9, 14, True, True],
+    [7088, 'Danforth/Coxwell', 43.683378, -79.322961, 15, 13, 2, False, False]]
+
+HANDOUT_STATIONS = [
+    [7000, 'Ft. York / Capreol Crt.', 43.639832, -79.395954, 31, 20, 11, True,
+     True],
+    [7001, 'Lower Jarvis St / The Esplanade', 43.647992, -79.370907,
+     15, 5, 10, True, True]]
+
+#########################################
+
+def clean_data(data: List[list]) -> None:
+    """Convert each string in data to an int if and only if it represents a
+    whole number, a float if and only if it represents a number that is not a
+    whole number, True if and only if it is 'True', False if and only if it is
+    'False', and None if and only if it is either 'null' or the empty string.
+
+    >>> d = [['abc', '123', '45.6', 'True', 'False']]
+    >>> clean_data(d)
+    >>> d
+    [['abc', 123, 45.6, True, False]]
+    >>> d = [['ab2'], ['-123'], ['False', '3.2']]
+    >>> clean_data(d)
+    >>> d
+    [['ab2'], [-123], [False, 3.2]]
+    """
+
+    for station in data:
+        for i in range(len(station)):
+            if station[i].strip().lstrip('-+').isnumeric():
+                station[i] = int(station[i])
+            elif is_number(station[i]):
+                station[i] = float(station[i])
+            elif station[i] == 'True':
+                station[i] = True
+            elif station[i] == 'False':
+                station[i] = False
+            elif station[i] == 'null' or station[i] == '':
+                station[i] = None
+
+
+def get_station_info(station_id: int, stations: List[list]) -> list:
+    """Return a list containing the following information from stations
+    about the station with id number station_id:
+        - station name
+        - number of bikes available
+        - number of docks available
+    (in this order)
+
+    Precondition: station_id will appear in stations.
+
+    >>> get_station_info(7087, SAMPLE_STATIONS)
+    ['Danforth/Aldridge', 9, 14]
+    >>> get_station_info(7088, SAMPLE_STATIONS)
+    ['Danforth/Coxwell', 13, 2]
+    """
+
+    i = 0
+
+    while stations[i][0] != station_id:
+        i += 1
+    return [stations[i][1], stations[i][5], stations[i][6]]
+
+
+def get_total(index: int, stations: List[list]) -> int:
+    """Return the sum of the column in stations given by index.
+
+    Precondition: the items in stations at the position
+                  that index refers to are ints.
+
+    >>> get_total(BIKES_AVAILABLE, SAMPLE_STATIONS)
+    22
+    >>> get_total(DOCKS_AVAILABLE, SAMPLE_STATIONS)
+    16
+    """
+
+    sum = 0
+
+    for i in range(len(stations)):
+        sum += stations[i][index]
+    return sum
+
+
+def get_station_with_max_bikes(stations: List[list]) -> int:
+    """Return the station ID of the station that has the most bikes available.
+    If there is a tie for the most available, return the station ID that appears
+    first in stations.
+
+    Precondition: len(stations) > 0
+
+    >>> get_station_with_max_bikes(SAMPLE_STATIONS)
+    7088
+    """
+
+    station_id_with_max_bikes = stations[0][0]
+    max_bikes = stations[0][5]
+
+    for station in stations:
+        if station[5] > max_bikes:
+            max_bikes = station[5]
+            station_id_with_max_bikes = station[0]
+    return station_id_with_max_bikes
+
+
+def get_stations_with_n_docks(n: int, stations: List[list]) -> List[int]:
+    """Return a list containing the station IDs for the stations in stations
+    that have at least n docks available, in the same order as they appear
+    in stations.
+
+    Precondition: n >= 0
+
+    >>> get_stations_with_n_docks(2, SAMPLE_STATIONS)
+    [7087, 7088]
+    >>> get_stations_with_n_docks(5, SAMPLE_STATIONS)
+    [7087]
+    """
+
+    min_required_docks_id = []
+
+    for station in stations:
+        if station[6] >= n:
+            min_required_docks_id.append(station[0])
+    return min_required_docks_id
+
+
+def get_direction(start_id: int, end_id: int, stations: List[list]) -> str:
+    """ Return a string that contains the direction to travel to get from
+    station start_id to station end_id according to data in stations.
+
+    Precondition: start_id and end_id will appear in stations.
+
+    >>> get_direction(7087, 7088, SAMPLE_STATIONS)
+    'SOUTHWEST'
+    """
+
+    direction_latitude = ''
+    direction_longitude = ''
+
+    for station in stations:
+        if station[0] == start_id:
+            start_latitude = station[2]
+            start_longitude = station[3]
+        if station[0] == end_id:
+            end_latitude = station[2]
+            end_longitude = station[3]
+
+    if start_latitude > end_latitude:
+        direction_latitude = 'SOUTH'
+    elif start_latitude < end_latitude:
+        direction_latitude = 'NORTH'
+    if start_longitude > end_longitude:
+        direction_longitude = 'WEST'
+    elif start_longitude < end_longitude:
+        direction_longitude = 'EAST'
+
+    return direction_latitude + direction_longitude
+
+
+def rent_bike(station_id: int, stations: List[list]) -> bool:
+    """Update the available bike count and the docks available count
+    for the station in stations with id station_id as if a single bike was
+    removed, leaving an additional dock available. Return True if and only
+    if the rental was successful.
+
+    Precondition: station_id will appear in stations.
+
+    >>> station_id = SAMPLE_STATIONS[0][ID]
+    >>> original_bikes_available = SAMPLE_STATIONS[0][BIKES_AVAILABLE]
+    >>> original_docks_available = SAMPLE_STATIONS[0][DOCKS_AVAILABLE]
+    >>> rent_bike(station_id, SAMPLE_STATIONS)
+    True
+    >>> original_bikes_available - 1 == SAMPLE_STATIONS[0][BIKES_AVAILABLE]
+    True
+    >>> original_docks_available + 1 == SAMPLE_STATIONS[0][DOCKS_AVAILABLE]
+    True
+    >>> station_id = SAMPLE_STATIONS[1][ID]
+    >>> original_bikes_available = SAMPLE_STATIONS[1][BIKES_AVAILABLE]
+    >>> original_docks_available = SAMPLE_STATIONS[1][DOCKS_AVAILABLE]
+    >>> rent_bike(station_id, SAMPLE_STATIONS)
+    False
+    >>> original_bikes_available == SAMPLE_STATIONS[1][BIKES_AVAILABLE]
+    True
+    >>> original_docks_available == SAMPLE_STATIONS[1][DOCKS_AVAILABLE]
+    True
+    """
+
+    for station in stations:
+        if station[0] == station_id:
+            if station[5] >= 1 and station[7]:
+                station[5] -= 1
+                station[6] += 1
+                return True
+    return False
+
+
+def return_bike(station_id: int, stations: List[list]) -> bool:
+    """Update the available bike count and the docks available count
+    for station in stations with id station_id as if a single bike was added,
+    making an additional dock unavailable. Return True if and only if the
+    return was successful.
+
+    Precondition: station_id will appear in stations.
+
+    >>> station_id = SAMPLE_STATIONS[0][ID]
+    >>> original_bikes_available = SAMPLE_STATIONS[0][BIKES_AVAILABLE]
+    >>> original_docks_available = SAMPLE_STATIONS[0][DOCKS_AVAILABLE]
+    >>> return_bike(station_id, SAMPLE_STATIONS)
+    True
+    >>> original_bikes_available + 1 == SAMPLE_STATIONS[0][BIKES_AVAILABLE]
+    True
+    >>> original_docks_available - 1 == SAMPLE_STATIONS[0][DOCKS_AVAILABLE]
+    True
+    >>> station_id = SAMPLE_STATIONS[1][ID]
+    >>> original_bikes_available = SAMPLE_STATIONS[1][BIKES_AVAILABLE]
+    >>> original_docks_available = SAMPLE_STATIONS[1][DOCKS_AVAILABLE]
+    >>> return_bike(station_id, SAMPLE_STATIONS)
+    False
+    >>> original_bikes_available == SAMPLE_STATIONS[1][BIKES_AVAILABLE]
+    True
+    >>> original_docks_available == SAMPLE_STATIONS[1][DOCKS_AVAILABLE]
+    True
+    """
+
+    for station in stations:
+        if station[0] == station_id:
+            if station[6] >= 1 and station[8]:
+                station[5] += 1
+                station[6] -= 1
+                return True
+    return False
+
+
+def balance_all_bikes(stations: List[list]) -> int:
+    """Calculate the percentage of bikes available across all stations
+    and evenly distribute the bikes so that each station has as close to the
+    overall percentage of bikes available as possible. Remove bikes from a
+    station if and only if the station is renting and there is a bike
+    available to rent, and return a bike if and only if the station is
+    allowing returns and there is a dock available. Return the difference
+    between the number of bikes rented and the number of bikes returned.
+
+    >>> balance_all_bikes(HANDOUT_STATIONS)
+    0
+    >>> HANDOUT_STATIONS == [\
+     [7000, 'Ft. York / Capreol Crt.', 43.639832, -79.395954, 31, 17, 14, \
+     True, True], \
+     [7001, 'Lower Jarvis St / The Esplanade', 43.647992, -79.370907, \
+     15, 8, 7, True, True]]
+    True
+    """
+
+    overall_percentage = get_total(5, stations) / get_total(4, stations)
+    num_rent = 0
+    num_return = 0
+
+    for station in stations:
+        adjusted_bikes = round(overall_percentage * station[4])
+        if adjusted_bikes > station[5] and station[8]:
+            if station[6] >= adjusted_bikes - station[5]:
+                num_return += adjusted_bikes - station[5]
+                station[6] -= adjusted_bikes - station[5]
+                station[5] = adjusted_bikes
+            else:
+                num_return += station[6]
+                station[5] += station[6]
+                station[6] = 0
+        elif adjusted_bikes < station[5] and station[7]:
+            num_rent += station[5] - adjusted_bikes
+            station[6] += station[5] - adjusted_bikes
+            station[5] = adjusted_bikes
+    return num_rent - num_return
+
+
+if __name__ == '__main__':
+    pass
